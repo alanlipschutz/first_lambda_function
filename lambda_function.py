3e43fff412e03076960c46b7f46726fccda9bb8f
alanlipschutz <alanelipschutz@gmail.com>
2023-06-15 17:56:12 +0300

    feat: added random.py script that move requirements to python folder, install requirements there, zip the folder, publish the layer, update the lambda function configuration to get the last version and update the lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -3,30 +3,30 @@ import urllib3
 import requests
 
 
-def lambda_handler(event, context):
-    # TODO implement
+def send_slack_message(event):
     http = urllib3.PoolManager()
-
-    slack_text = f"{event['Records']}"
-
+    slack_text = f"{event}"
     print(slack_text)
-
     data = {"text": slack_text}
-
     request = http.request("POST",
-                           "https://hooks.slack.com/services/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",
+                           "https://hooks.slack.com/services/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",
                            body=json.dumps(data),
                            headers={"Content-Type": "application/json"})
 
+def send_whatsapp_message(event, number):
     url = "https://api.ultramsg.com/instance50806/messages/chat"
-
-    payload = f"token=vi9d9iw5e07ez32c&to=+972584164863&body={event['Records']}"
+    payload = f"token=vi9d9iw5e07ez32c&to={number}&body={event}"
     payload = payload.encode('utf8').decode('iso-8859-1')
     headers = {'content-type': 'application/x-www-form-urlencoded'}
 
     response = requests.request("POST", url, data=payload, headers=headers)
     print(response.text)
-    print(event)
+
+def lambda_handler(event, context):
+    # TODO implement
+    phone_number = "+972584164863"
+    send_slack_message(event)
+    send_whatsapp_message(event, phone_number)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
---
+++ random.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+this_lambda_arn = 'arn:aws:lambda:eu-north-1:960346235350:function:send_notification'
+this_layer_arn = 'arn:aws:lambda:eu-north-1:960346235350:layer:send_notification_layer'
+file_path = "lambda_function.py"
+compressed_file_path = "python.zip"
+
+os.system('cp ./requirements.txt ./python/requirements.txt')
+os.system('pip install -r ./python/requirements.txt')
+os.system('zip -r layer.zip python')
+os.system(f'aws lambda publish-layer-version --layer-name {this_layer_arn} --description "send notification '
+          f'dependencies" --license-info "MIT" --zip-file fileb://layer.zip --compatible-runtimes python3.10')
+lambda_conf = os.popen(f'aws lambda list-layer-versions --layer-name {this_layer_arn} --query "LayerVersions['
+                       f'0].LayerVersionArn" --output text').read()
+last_layer_version = lambda_conf.split("\n")[0]
+os.system(f"zip -r -u '{compressed_file_path}' '{file_path}'")
+os.system(f'aws lambda update-function-configuration --function-name {this_lambda_arn} --layers {last_layer_version}')
+# give 20 seconds to let lambda function update his configuration
+time.sleep(20)
+update_command = f"aws lambda update-function-code --function-name {this_lambda_arn} --zip-file fileb://{compressed_file_path} --publish"
+os.system(update_command)
